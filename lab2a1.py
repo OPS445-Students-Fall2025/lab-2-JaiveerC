09285da47313aa239bbb8d5218ae461aa5bfc789
Jaiveer Chawla <jchawla3@myseneca.ca>
2025-09-19 21:26:08 -0400

    Added new files

---
+++ lab2a1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+name = 'Jon'
+age = 20
+colour = input("Type in a colour and press enter: ")
+print('The color I typed in is: ' + colour)
---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author:Jaiveer Chawla
+#Author ID:101618205
+#Date Created:2025/09/19
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer!=0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
